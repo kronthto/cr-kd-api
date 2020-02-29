9c5e2ecb9f750f23ccac97f8182054ffbdec1db8
Tobias Kronthaler <kronthto@gmail.com>
2020-02-29 18:17:32 +0100

    Error handling

--- app/actions.js
+++ app/actions.js
@@ -1,24 +1,22 @@
 const { validationResult } = require('express-validator')
 const db = require('./db')
 
-const killsViaTimeRange = function(req, res) {
+const killsViaTimeRange = function(req, res, next) {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() })
   }
 
-  db.killsViaTimeRangeQuery(
-    req.query.from,
-    req.query.to,
-    req.query.group || 60
-  ).then(dbRes => {
-    res.json(
-      dbRes.rows.map(row => {
-        row.killcount = Number(row.killcount)
-        return row
-      })
-    )
-  })
+  db.killsViaTimeRangeQuery(req.query.from, req.query.to, req.query.group || 60)
+    .then(dbRes => {
+      res.json(
+        dbRes.rows.map(row => {
+          row.killcount = Number(row.killcount)
+          return row
+        })
+      )
+    })
+    .catch(e => next(e))
 }
 
 module.exports = { killsViaTimeRange }
